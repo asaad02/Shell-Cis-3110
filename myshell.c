48a335efd84de0713547d06ec407eb77d52abb2b
Abdullah Saad <asaad02@uoguelph.ca>
2021-01-19 19:59:29 -0500

    first day

--- myshell.c
+++ myshell.c
@@ -7,27 +7,38 @@
  **/
 
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+find1_ampersand(arguments,*arguments_number);
 
 #define LINE 80
 #define DELIMITERS  " \t\n\v\f\r"
 
-taking_command(char *command){
+void taking_command(char *command){
     /* strcpy() function copies the string pointed by "" (including the null character) to the Command */
     strcpy(command,"");
 }
 
 char input( char *command){
 
+    // buffer hold the command 
     char buffer[LINE +1];
 
+    // get the command 
     if(fgets(buffer, LINE , stdin) == NULL){
-        printf(stderr," Failed to read the command ! \n");
+        fprintf(stderr," Failed to read the command ! \n");
         return 0;
     }
     // The null pointer constant NULL's value equals 0
     if(strncmp(buffer,"!!",2)==0 ){
         if(strlen(command) == 0){
-            printf(stderr," No history avaliable ! \n");
+            fprintf(stderr," No history avaliable ! \n");
             return 0;
         }
         printf("%s",command);
@@ -45,12 +56,36 @@ void saving_arguemnts(char *arguments []){
     }
 }
 
+int run(char **arguments, int arguments_number){
+
+    // executed in the background using &
+    int run_background = find1_ampersand(arguments,&arguments_number);
+    //pid_t data type stands for process identification and it is used to represent process ids.
+    //child process  
+    pid_t child_id = fork();
+
+    if(child_id < 0){
+        //prints a descriptive error message to stderr
+        fprintf(stderr, "error: fork error\n");
+		return 0;
+        
+
+    }
+}
+
+int find1_ampersand( char **arguments, int*size){
+
+
+return 0;
+
+}
+
 
 int main(void){
 
-    char *arguments [LINE +1];
     char command[LINE +1];
-
+    char *arguments [LINE +1];
+    
     saving_arguemnts(arguments);
     taking_command(command);
 
@@ -71,8 +106,10 @@ int main(void){
 
         }
         // Set 1 FunctionsThe system calls that are listed are suggestions –you must decide what is appropriate.
-        if ((strcmp(arguments[0], "exit") == 0) || (strcmp(arguments[0], "Exit") == 0)){
+        if ((strcmp(&command[0], "exit") == 0) || (strcmp(&command[0], "Exit") == 0)){
             //System calls: exit()
+            fputs("myShell terminating..... ", stdout);
+            fputs("[Process completed] ", stdout);
             exit(0);
         }
 
